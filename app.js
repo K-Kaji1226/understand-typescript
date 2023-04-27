2bee889dd85c1165861d4a6feecae0e8efd4de44
K-Kaji1226 <rashsea1127@gmail.com>
2023-04-28 07:27:35 +0900

    Literal型、型エイリアスについて学びました。

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combineAges = combine(20, 30);
+var combineAges = combine(20, 30, "as-number");
 console.log(combineAges);
-var combineNames = combine("Max", "Anna");
+var combineStringAges = combine("25", "45", "as-number");
+console.log(combineStringAges);
+var combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
-function combine(input1: number | string, input2: number | string) {
+// 型エイリアス
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+	input1: Combinable,
+	input2: Combinable,
+	resultConversion: ConversionDescriptor
+) {
 	let result;
-	if (typeof input1 === "number" && typeof input2 === "number") {
-		result = input1 + input2;
+	if (
+		(typeof input1 === "number" && typeof input2 === "number") ||
+		resultConversion === "as-number"
+	) {
+		result = +input1 + +input2;
 	} else {
 		result = input1.toString() + input2.toString();
 	}
 	return result;
 }
 
-const combineAges = combine(20, 30);
+const combineAges = combine(20, 30, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Max", "Anna");
+const combineStringAges = combine("25", "45", "as-number");
+console.log(combineStringAges);
+
+const combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
